36e74819fce8df6bb73b6523fc7000f37343c49d
Alexander <a1ex.chaos.exe@gmail.com>
2015-02-05 14:27:28 +0500

    Added global catching unexpected exceptions

--- DocumentDb/App.xaml.cs
+++ DocumentDb/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Data.Entity;
 using System.IO;
 using System.Windows;
+using System.Windows.Threading;
+using Common.Utils;
 using DataLayer;
+using DataLayer.Migrations;
 using DocumentDb.Common;
 
 namespace DocumentDb
@@ -17,9 +20,17 @@ namespace DocumentDb
                 Directory.CreateDirectory("data");
             }
 
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DdbContext, DataLayer.Migrations.Configuration>());
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DdbContext, Configuration>());
             ApplicationWorkers.DirectoryMonitor.Update();
             ApplicationWorkers.DocumentParser.Start();
+
+            DispatcherUnhandledException += OnDispatcherUnhandledException;
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            Logger.Instance.Error("Необработанное исключение: {0}", (object)args.Exception);
+            args.Handled = true;
         }
     }
 }
