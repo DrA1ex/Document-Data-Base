f46d06e042f841573636673a26ac4b30db0d0d81
Alexander <a1ex.chaos.exe@gmail.com>
2015-02-07 13:28:20 +0500

    Release 0.3

--- DocumentDb/Properties/AssemblyInfo.cs
+++ DocumentDb/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
